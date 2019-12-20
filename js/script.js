31bff2381bba3f430abb642491a64ef7e057dc6d
Mohamed Amara <20605958+mohamedamara@users.noreply.github.com>
2019-12-20 10:49:35 +0100

    add new files

---
+++ js/script.js
@@ -0,0 +1,22 @@
+(function($) {
+  "use strict";
+  $('a.scroll-link[href*="#"]:not([href="#"])').click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 55
+          },
+          800
+        );
+        return false;
+      }
+    }
+  });
+})(jQuery);
